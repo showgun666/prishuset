85b1b5640bd14af3487d888ee1dd04ff1c1c767d
Markus Lindgren <mavn22@bth.se>
2023-10-08 14:05:45 +0200

    added tests for pricing()

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 import pytest
 import helpers as h
+import jetshop as j
 
 def test_roundNearest():
     a = h.roundNearest(123.449) # 123
@@ -39,3 +40,18 @@ def test_rounding():
     bf = h.rounding(123456789, "nearest", 10) # 123456790
 
     assert a == 14 and b == 14 and c == 15 and ab == 145 and ac == 145 and ad == 140 and ae == 145 and af == 10 and ah == 145 and ai == 150 and ba == 1000 and bb == 995 and bc == 1000 and bd == 1010 and be == 1440 and bf == 123456790
+
+def test_pricing():
+    a = j.pricing(10.4, 1.11)[0] # 15
+    b = j.pricing(5, 1.11)[0] # 7
+    c = j.pricing(1, 1.11)[0] # 2
+    d = j.pricing(136.88, 1.11)[0] # 190
+    e = j.pricing(190, 1.11)[0] # 190
+    
+    aa = j.pricing(255.55, 1.11)[0] # 355
+    ab = j.pricing(574.4, 1.11)[0] # 795
+    ac = j.pricing(999, 1.11)[0] # 1390
+    ad = j.pricing(555.555, 1.11)[0] # 770
+    ae = j.pricing(444.444, 1.11)[0] # 615
+
+    assert a == 15 and b == 7 and c == 2 and d == 190 and e == 265 and aa == 355 and ab == 795 and ac == 1390 and ad == 770 and ae == 615
